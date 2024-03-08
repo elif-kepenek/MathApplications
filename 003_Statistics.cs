1641959f805e2b915fa9d7a41e0a777632b11ba2
Elif Kepenek <elif.kepenek.98@gmail.com>
2024-03-08 19:14:01 +0300

    create data

--- 003_Statistics.cs
+++ 003_Statistics.cs
@@ -1,6 +1,17 @@
 namespace MathAplications
 {
     public class Statistics {
-        
+        public static void CreateData () {
+            Console.WriteLine("Kaç veriden oluşacak? ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[limit];
+
+            for (int i = 0; i < limit; i++)
+            {
+                numbers[i] = new Random().Next(0, 100);
+                Console.Write("{0, 3}", numbers[i]);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,7 @@
     class Program
     {
         static void Main (string[] args) {
-
-            double r = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Alan: {0:F2}", Circle.AreaOfTheCircle(r, 60));
+            Statistics.CreateData();
         }
     }
     
