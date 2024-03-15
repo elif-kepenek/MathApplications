32fbca5b722c03b25332494672cf45958abfad2b
Elif Kepenek <elif.kepenek.98@gmail.com>
2024-03-15 21:58:14 +0300

    statistics explanation

--- 003_Statistics.cs
+++ 003_Statistics.cs
@@ -1,6 +1,9 @@
 namespace MathAplications
 {
     public class Statistics {
+        /// <summary>
+        /// Girilen sayı kadar elamanlı 1-100 arasındaki sayılardan dizi oluşturur.
+        /// </summary>
         public static void CreateData () {
             Console.WriteLine("Kaç veriden oluşacak? ");
             int limit = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +18,11 @@ namespace MathAplications
             Console.WriteLine("\nDizinin en büyük elemanı: {0} \nDizinin en küçük elemanı: {1}", BiggestData(numbers), SmallestData(numbers));
         }
 
+        /// <summary>
+        /// Sayılardan oluşan dizinin en büyük elemanını döner.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
         public static int BiggestData (int[] numbers) {
             int bigData = numbers[0];
             for (int i = 0; i < numbers.Length; i++)
@@ -27,6 +35,11 @@ namespace MathAplications
             return bigData;
         }
 
+        /// <summary>
+        /// Sayılardan oluşan dizinin en küçük elemanını döner.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
         public static int SmallestData (int[] numbers) {
             int smallData = numbers[0];
             for (int i = 0; i < numbers.Length; i++)
