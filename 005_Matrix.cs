ce5ec446eae7616a5ff161c2690b8cb448c7ade9
Elif Kepenek <elif.kepenek.98@gmail.com>
2024-03-26 22:39:33 +0300

    Revert "is it identity matrix"
    This reverts commit 8f0797c995bf80245a428f789c2b2652f0de024e.

--- 005_Matrix.cs
+++ 005_Matrix.cs
@@ -107,11 +107,6 @@ namespace MathAplications
             return ScalarMatrix(dimension, 1);
         }
 
-        /// <summary>
-        /// Birim matris olup olmadığını kontrol eder.
-        /// </summary>
-        /// <param name="M">Matris</param>
-        /// <returns>Birim matris ise true birim matris değil ise false</returns>
         public static bool IsItIdentityMatrix (int[,] M) {
             bool control = true;
             for (int i = 0; (i < M.GetLength(0) && control == true); i++)
