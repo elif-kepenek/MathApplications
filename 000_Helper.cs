43249b441669aba3e8f386e591484707948d2648
Elif Kepenek <elif.kepenek.98@gmail.com>
2024-03-28 19:39:44 +0300

    helper created and print error func

---
+++ 000_Helper.cs
@@ -0,0 +1,20 @@
+namespace MathAplications
+{
+    public class Helper {
+
+        /// <summary>
+        /// Hata mesajının konsolda renkli görünmesini sağlar.
+        /// </summary>
+        /// <param name="message">Mesaj</param>
+        public static void PrintError (string message) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\a");
+            Console.WriteLine(message);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        
+    }
+}
