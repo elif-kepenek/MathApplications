97d11347235a125ea4ffee7f72746da19b1b8b59
Elif Kepenek <elif.kepenek.98@gmail.com>
2024-04-22 20:39:40 +0300

    :

--- Program.cs
+++ Program.cs
@@ -4,11 +4,6 @@
     {
         static void Main (string[] args) {
             
-            int[,] Matris = Matrix.LowerTriangularMatrix();
-
-            Matrix.PrintMatrix(Matris);
-
-            Console.WriteLine("{0}", Matrix.IsItLowerTriangularMatrix(Matris) ? "Alt üçgen matris" : "Alt üçgen matris değil");
         }
     }
     
